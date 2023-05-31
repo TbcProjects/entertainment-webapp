74941c0a4d77f9083b57286e3e26d5eeac95079f
TbcProjects <daniel@thebikeclub.co.uk>
2023-05-31 08:57:04 +0100

    Test chakra ui elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { Box, Text } from "@chakra-ui/react";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Box>
+        <Text>Chakra UI Test</Text>
+      </Box>
+    </main>
+  );
 }
